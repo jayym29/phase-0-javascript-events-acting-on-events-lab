e9ada79297365bcac30794e3d8a5c994a3d712a8
jayymccants@gmail.com <jayymccants@gmail.com>
2024-03-20 20:44:17 -0400

    Implement dodger movement and boundary checks

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const left = dodger.style.left.replace('px', '');
+  const leftNumber = parseInt(left, 10);
+
+
+  if (leftNumber > 0) {
+    dodger.style.left = `${leftNumber - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = dodger.style.left.replace('px', '');
+  const leftNumber = parseInt(left, 10);
+
+  const gameFieldWidth = window.innerWidth - 40; 
+  if (leftNumber + 40 >= gameFieldWidth) {
+    return;
+  }
+
+  dodger.style.left = `${leftNumber + 1}px`;
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
